d85d55e7a6051140184bd53cc64a193793d8e4b1
Manish Madhwani <manishmadhwani6@gmail.com>
2026-03-05 11:35:11 +0530

    Godown_Management_AddItems_ByUser: removed add items functionality from the application

--- src/main/java/org/godownManagement/repository/ItemRepository.java
+++ src/main/java/org/godownManagement/repository/ItemRepository.java
@@ -2,16 +2,9 @@ package org.godownManagement.repository;
 
 import org.godownManagement.entities.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Integer> {
 
-    @Query(name = "GetAllItemsPerUser",
-            value = "SELECT items FROM Item items where items.owner.userName = :userName")
-    List<Item> getAllItemsPerUser(@Param("userName") String userName);
 }
